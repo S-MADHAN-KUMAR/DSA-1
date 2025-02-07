3733bf9b1d570028cea2e4969bff81dce7c475e3
Madhan Kumar <madhankumar4195@gmail.com>
2025-02-07 12:03:33 +0530

    Update CoutnDown.js

--- Recursion/CoutnDown.js
+++ Recursion/CoutnDown.js
@@ -1,9 +1,9 @@
 function countDown(n) {
     if (n < 0) {
-        return; // Base case
+        return; 
     }
-    console.log(n); // Print the current number
-    countDown(n - 1); // Recursive case
+    console.log(n); 
+    countDown(n - 1); 
 }
 
 countDown(5); // Output: 5, 4, 3, 2, 1, 0
